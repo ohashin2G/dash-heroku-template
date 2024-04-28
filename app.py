bd3c1ff70bc923141f9a43f959626aef46c7d74c
ohashin2G <ohashin2@gmail.com>
2024-04-27 23:35:54 -0400

    Hashed old dashes

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import dash
 from jupyter_dash import JupyterDash
-import dash_core_components as dcc
-from dash import dcc
-import dash_html_components as html
-from dash import html
+#import dash_core_components as dcc
+#from dash import dcc
+#import dash_html_components as html
+#from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
